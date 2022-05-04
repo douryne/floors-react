1fbda70d3d37b13a23799e48422f020ab73de9a7
douryne <douryne.1st@gmail.com>
2022-05-04 17:50:41 +0500

    refactor: add image to apart component and change styles

--- src/components/ApartComponent/ApartComponent.jsx
+++ src/components/ApartComponent/ApartComponent.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import classes from './ApartComponent.module.css';
 
-const ApartComponent = ({id, area_total, floor, rooms, price}) => {
+const ApartComponent = ({id, area_total, floor, rooms, price, img}) => {
   return (
     <div className={classes.apart}>
-      <h2 className={classes.heading}>
-        Квартира: <span className={classes.id}>{id}</span>
-      </h2>
-      <ul className={classes.list}>
-        <li>Total Area: {area_total}</li>
-        <li>Floor: {floor} </li>
-        <li>Rooms: {rooms}</li>
-      </ul>
-      <span className={classes.price}>Price: {price}</span>
+      <div className={classes.text_content}>
+        <h2 className={classes.heading}>
+          Квартира: <span className={classes.id}>{id}</span>
+        </h2>
+        <ul className={classes.list}>
+          <li>Total Area: {area_total}</li>
+          <li>Floor: {floor} </li>
+          <li>Rooms: {rooms}</li>
+        </ul>
+        <span className={classes.price}>Price: {price}</span>
+      </div>
+      <img className={classes.img} src={img} alt="" />
     </div>
   );
 };

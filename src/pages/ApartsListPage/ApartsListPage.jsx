8fce685d1845a57768ddffe713f4929166a2d1eb
douryne <douryne.1st@gmail.com>
2022-05-05 15:05:18 +0500

    feature: pagination

--- src/pages/ApartsListPage/ApartsListPage.jsx
+++ src/pages/ApartsListPage/ApartsListPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import ApartsList from '../../components/ApartsList/ApartsList';
 import ApartsSort from '../../components/ApartsSort/ApartsSort';
 import { useSortedAparts } from '../../hooks/useSortedAparts';
@@ -10,18 +10,33 @@ import classes from './ApartsListPage.module.css';
 const ApartsListPage = () => {
   const [apartments, setApartments] = useState([]);
   const [selectedSort, setSelectedSort] = useState({sortType: '', from: ''});
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const apartsToShow = 4;
 
   const [fetchAparts, isLoading, error] = useFetching(async () => {
     const apartamentsRes = await fetchApartaments();
     setApartments(apartamentsRes);
+    setTotalPage(Math.ceil(apartamentsRes.length/apartsToShow));
   });
   
   useEffect(() => {
     fetchAparts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []) 
+  }, [])
+
+  useMemo(() => {
+    setPage(1);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedSort])
 
   const sortedAparts = useSortedAparts(apartments, selectedSort);
+
+  const paginatedAndSortedAparts = useMemo(() => {
+    if (!sortedAparts) return [];
+    return [...sortedAparts].slice((page-1)*apartsToShow, page*apartsToShow);
+  }, [page, sortedAparts]);
+
 
   const sortContent = {
     sortTypes: [
@@ -49,6 +64,11 @@ const ApartsListPage = () => {
         selectedSort={selectedSort}
         setSelectedSort={setSelectedSort}
       />
+      <div style={{marginBottom: '20px', display: 'flex', gap: '20px'}}>
+        <span className={classes.heading}>{page}</span>
+        <button disabled={page === 1} onClick={() => setPage(page-1)}>Prev</button>
+        <button disabled={page === totalPage} onClick={() => setPage(page+1)}>Next</button>
+      </div>
       {
         isLoading ? (
           <h2>Loading...</h2>
@@ -56,7 +76,7 @@ const ApartsListPage = () => {
           error ? (
             <h2>{error}</h2>
           ) : (
-            <ApartsList sortedAparts={sortedAparts} />
+            <ApartsList sortedAparts={paginatedAndSortedAparts} />
           )
         )
       }
